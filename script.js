2eb5788c3713e3d3aea777c1921cdb7a25443ff1
geovannifranca <geovannidefranca98@gmail.com>
2023-07-04 20:41:49 -0300

    Menu hambuguer adicionado

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector('.menuMobile').addEventListener('click', function(){
+    if(document.querySelector('.menu nav ul').style.display == 'flex'){
+        document.querySelector('.menu nav ul').style.display = 'none';
+    } else {
+        document.querySelector('.menu nav ul').style.display = 'flex'
+    }
+
+});
